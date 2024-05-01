5a28e8d3cf5c6673c58646ee89def2a6a7b49a2e
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2024-05-01 12:25:34 +0200

    Added download button functionality

--- siteHandler.js
+++ siteHandler.js
@@ -1,4 +1,6 @@
 var link = "";
+var add_button = true;
+var instant_download = true;
 
 function refreshLink() {
   link = document.querySelector(".pb-video-player").getAttribute("src");
@@ -9,12 +11,16 @@ function addButton() {
   newButton.classList.add("bttn", "bttn-light", "bttn-light-block");
   newButton.style.marginBottom = "4px";
   newButton.style.backgroundColor = "#d68526";
-  newButton.innerHTML = "Download";
+  linkTag = "<a style=\"color: #24282a;\" href=\"" + link + "\" target=\"_blank\" ";
+  if (instant_download) // this currently does not work, because of same-origin policy
+    linkTag += "download";
+  linkTag += ">Download</a>";
+  newButton.innerHTML = linkTag;
 
   let boxButtons = document.querySelector(".box-buttons");
   boxButtons.insertBefore(newButton, boxButtons.childNodes[4])
 }
 
 refreshLink();
-console.log(link);
-addButton();
+if (add_button)
+  addButton();
