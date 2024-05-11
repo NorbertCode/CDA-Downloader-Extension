b69a417863a55f10d34300b41e4fd623e869cdac
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2024-05-11 13:45:16 +0200

    Checkboxes' values are now saved in local storage

--- popup.js
+++ popup.js
@@ -23,19 +23,36 @@ function loadOptions() {
 
   chrome.storage.local.get(["dataExists"]).then((result) => {
     if (result.dataExists == "true") {
-      chrome.storage.local.get(["add_button"]).then((result) => { addButton.checked = (result.add_button === true); });
-      chrome.storage.local.get(["instant_download"]).then((result) => { instantDownload.checked = (result.instant_download === true); });
+      chrome.storage.local.get(["add_button"]).then((result) => { addButton.checked = (result.add_button == "true"); });
+      chrome.storage.local.get(["instant_download"]).then((result) => { instantDownload.checked = (result.instant_download == "true"); });
     }
     else {
       chrome.storage.local.set({ "dataExists": "true" });
 
-      chrome.storage.local.set({ "add_button": addButton.checked });
-      chrome.storage.local.set({ "instant_download": instantDownload.checked });
+      chrome.storage.local.set({ "add_button": addButton.checked.toString() });
+      chrome.storage.local.set({ "instant_download": instantDownload.checked.toString() });
     }
   });
 }
 
+function changeValue(element) {
+  id = element.id;
+  checked = element.checked.toString();
+  chrome.storage.local.set({ [id]: checked }).then(() => { chrome.storage.local.get(id).then((result) => { console.log(id, result); }); });
+}
+
+function setInputEvents() {
+  let elements = document.querySelectorAll('input[type="checkbox"]');
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", () => { changeValue(elements[i]); });
+  }
+}
+
+setInputEvents();
 loadOptions();
+
+chrome.storage.local.get("dataExists").then((result) => { console.log(result); });
+chrome.storage.local.get("add_button").then((result) => { console.log(result); });
 
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   let activeTab = tabs[0];
