b2ce758072ae981b9c2845237f040a96f31da351
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2024-05-16 22:34:56 +0200

    Implement saving filename option

--- background.js
+++ background.js
@@ -3,9 +3,14 @@
 // This is the only way to instantly start a download from the content script, because cda.pl holds .mp4s on a different origin.
 
 function download(link) {
-  chrome.storage.local.get(["instant_download"]).then((result) => {
-    if (result.instant_download == "true")
-      chrome.downloads.download({ url: link });
+  chrome.storage.local.get(["instant_download", "file_name"]).then((result) => {
+    let saveAs = result.file_name == "custom";
+    if (result.instant_download == "true") {
+      chrome.downloads.download({
+        url: link,
+        saveAs: saveAs
+      });
+    }
     else
       chrome.tabs.create({ url: link });
   });
